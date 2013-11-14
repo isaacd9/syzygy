13dd2ce2e4a50d2e90bb7c72008da944677ab239
Isaac Diamond <isaacd9@gmail.com>
2013-11-14 10:19:53 -0800

    [Master] Boilerplate code for auto. Does not currently build as this depends on definition that will go in auto_main.c

---
+++ auto_common.c
@@ -0,0 +1,59 @@
+//definitions
+#define DRIVE_ENCODERS abs(nMotorEncoder[DRIVE_LEFT])+abs(nMotorEncoder[DRIVE_RIGHT])/2
+#define ARM_ENCODERS abs(nMotorEncoder[ARM_LEFT])+abs(nMotorEncoder[ARM_RIGHT])/2
+
+//includes
+#include "drivetrain2.h"
+#include "flag_raiser.h"
+#include "arm.h"
+
+//Function prototypes
+void moveDriveTicks(int speed, int numTicks);
+void turnDriveTicks(int speed, int numTicks); //To turn right, speed is <0
+void turnDrive45(); //Cannot set speeds for this as the speeds need to be preset for a predictable angle
+void turnDrive90();
+void turnDrive180();
+void turnDrive360();
+void stopDrive();
+
+void moveArmTicks(int speed, int numTicks);
+
+//Function bodies
+
+void moveDriveTicks(int speed, int numTicks) {
+	while(DRIVE_ENCODERS < numTicks) {
+		_setDriveMotors(speed,speed);
+	}
+}
+
+void turnDriveTicks(int speed, int numTicks) {
+	while(DRIVE_ENCODERS < numTicks) {
+		_setDriveMotors(speed,-speed);
+	}
+}
+
+void turnDrive45() {
+	turnDriveTicks(TURN_SPEED, FORTY_FIVE_TICKS);
+}
+
+void turnDrive90() {
+	turnDriveTicks(TURN_SPEED, NINETY_TICKS);
+}
+
+void turnDrive180() {
+	turnDriveTicks(TURN_SPEED, ONE_EIGHTY_TICKS);
+}
+
+void turnDrive360() {
+	turnDriveTicks(TURN_SPEED, THREE_SIXTY_TICKS);
+}
+
+void stopDrive() {
+	_setDriveMotors(0,0);
+}
+
+void moveArmTicks(int speed, int numTicks) {
+	while(ARM_ENCODERS < numTicks) {
+		_setArmMotors(speed, speed);
+	}
+}
