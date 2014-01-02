f2ecb995a1c893549807d15117ae9366529caba3
Richard Vuu <richardvuv@live.com>
2014-01-02 14:46:48 -0800

    dead reck for right side. change orientation

--- first_bucket_right.c
+++ first_bucket_right.c
@@ -62,7 +62,7 @@ static const bool USE_LOG_SCALE=true;
 #define BUCK4_TICKS 5200
 #define BUCK_WINDOW_TICKS 40
 
-#define ARM_TIME 2200
+#define ARM_TIME 2300
 #define KICK_TIME 1000
 
 #define END_OF_LINE 5300
@@ -92,23 +92,23 @@ task main () {
 	//runDriveTime(DRIVE_SPEED, 500);
 
 	runArmTime(ARM_SPEED, ARM_TIME);
-	runDriveTime(DRIVE_SPEED, 1000);
+	runDriveTime(DRIVE_SPEED, 1100);
 	wait1Msec(300);
 	//runArmTime(-ARM_SPEED, 600);
 	flapOpen();
 	runIntakeTime(INTAKE_SPEED, KICK_TIME);
 	flapClosed();
 	//runArmTime(ARM_SPEED, 600);
-	runDriveTime(-DRIVE_SPEED, 1000);
+	runDriveTime(-DRIVE_SPEED, 850);
 	runArmTime(-ARM_SPEED, ARM_TIME-200);
 
-	turnTime(TURN_SPEED, 500);
+	turnTime(TURN_SPEED, 800);
 	//turnDriveLeftTicks(TURN_SPEED, DRIVE_ENCODERS-(NINETY_DEGREES/2)-100);
 
 	zeroEncoders();
 
-	for (int i=4750; i>=0; i=i-5) {
-		_setDriveMotors(DRIVE_SPEED, DRIVE_SPEED-5);
+	for (int i=4500; i>=0; i=i-5) {
+		_setDriveMotors(DRIVE_SPEED, DRIVE_SPEED-10);
 		_setIntakeMotor(100);
 		wait1Msec(5);
 	}
@@ -118,11 +118,11 @@ task main () {
 	flapClosed();
 
 	for (int i=750; i>=0; i=i-5) {
-		_setDriveMotors(-DRIVE_SPEED+5, -DRIVE_SPEED);
+		_setDriveMotors(-DRIVE_SPEED, -DRIVE_SPEED-10);
 		wait1Msec(5);
 	}
 
-	turnTime(TURN_SPEED, 400);
-	runDriveTime(-100, 2750);
+	turnTime(TURN_SPEED, 500);
+	runDriveTime(-100, 2500);
 
 }
